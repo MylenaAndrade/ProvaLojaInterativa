4735a2ee5ba13da39006d923d41a2d97664ff289
Mylena <mylenaandrade200@gmail.com>
2023-09-15 15:08:14 -0300

    Inserção de produtos com o php.
    Pegando os dados através do método GET e inserindo na tabela as informações.

--- php/inserirProduto.php
+++ php/inserirProduto.php
@@ -1,5 +1,21 @@
 <?php
     include("conexao.php");
 
-    
+    if($_SERVER["REQUEST_METHOD"] == "GET"){
+        $nome = $_GET["nome"];
+        $preco = $_GET["preco"];
+        $quantidade = $_GET["quantidade"];
+        $idCategoria = $_GET["idCategoria"];
+
+        $sql ="INSERT INTO produtos VALUES(default,'$nome','$preco','$quantidade','$idCategoria')";
+
+        if(mysqli_query($mysqli, $sql)){
+            echo "Produto cadastrado com sucesso";
+        }else{
+            echo "Erro!" . mysqli_connect_error($mysqli);
+        }
+
+        
+        mysqli_close($mysqli);
+    }
 ?>
