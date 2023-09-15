25926dcae49a9995352288cfee2559cd2e6fb721
Mylena <mylenaandrade200@gmail.com>
2023-09-15 15:19:35 -0300

    Remoção de uma categoria.

--- php/removerCategoria.php
+++ php/removerCategoria.php
@@ -1,4 +1,15 @@
 <?php
     include_once("conexao.php");
 
+    if($_SERVER["REQUEST_METHOD"] == "GET"){
+    
+        $id=$_GET["id"];
+
+        $query = "DELETE FROM categorias WHERE id = '$id'";
+
+        mysqli_query($mysqli,$query);
+
+        mysqli_close($mysqli);
+
+    }
 ?>
