bd96eaf465916d5c1912a0c0bda021249eb94e9c
Mylena <mylenaandrade200@gmail.com>
2023-09-15 14:57:45 -0300

    Inserção de categorias com o php.
    Pegando as informações através do método GET e inserindo na tabela as informações.

--- php/inserirCategoria.php
+++ php/inserirCategoria.php
@@ -1,5 +1,20 @@
 <?php
     include("conexao.php");
 
+    if($_SERVER["REQUEST_METHOD"] == "GET"){
+        $nome = $_GET["nome"];
+        $dtCriacao = $_GET["dtCriacao"];
+
+        $sql ="INSERT INTO categorias VALUES(default,'$nome',default)";
+
+        if(mysqli_query($mysqli, $sql)){
+            echo "Categoria cadastrada com sucesso";
+        }else{
+            echo "Erro!" . mysqli_connect_error($mysqli);
+        }
+
+        
+        mysqli_close($mysqli);
+    }
 
 ?>
